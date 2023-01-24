b8d0179e57f74995c7744160c28575d7a35d2904
Michael <michaelhany101@gmail.com>
2023-01-24 14:07:23 +0200

    add HelloITI.swift

---
+++ HelloITI.swift
@@ -0,0 +1,15 @@
+//
+//  HelloITI.swift
+//  
+//
+//  Created by Michael Hany on 24/01/2023.
+//
+
+import Foundation
+class HelloITI
+{
+    func printing()
+    {
+        print("Hello ITI")
+    }
+}
---
+++ HelloWorld.swift
@@ -0,0 +1,15 @@
+//
+//  HelloWorld.swift
+//  
+//
+//  Created by Michael Hany on 24/01/2023.
+//
+
+import Foundation
+class HelloWorld
+{
+    func printing()
+    {
+        print("Hello World")
+    }
+}
